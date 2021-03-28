96ddeb53178b88560dde96b681708e76a8b1b908
Roberto Borbolla <irob263@gmail.com>
2021-03-28 01:39:57 -0600

    Agregar archivos de proyecto.

---
+++ Product/Camera/ICamera.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CameraReview.Product.Camera
+{
+    public interface ICamera: IProduct
+    {
+        public int maxISO { get; set; }
+        public string Type { get; set; }
+        public int CropFactor { get; set; }
+    }
+}
